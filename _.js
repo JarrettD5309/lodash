250930d63757958e843168454d4b3a68087ae297
Jarrett Dougherty <jarrettdougherty@gmail.com>
2020-10-16 14:32:35 -0400

    finished words and pad

--- _.js
+++ _.js
@@ -47,6 +47,47 @@ const _ = {
         };
         const isInRange = start<=number&&end>number;
         return isInRange;
+    },
+    // My Solution
+    // words(str) {
+    //     return str.split(" ");
+    // },
+    // Codecademy Solution
+    words(string) {
+        const words = string.split(' ');
+        return words;
+    },
+    // My Solution
+    // pad(str,strLength) {
+    //     if (str.length>=strLength) {
+    //         return str;
+    //     } else {
+    //         const extraPadding = strLength-str.length;
+    //         let newString = str;
+    //         if (extraPadding%2===0) {
+    //             const halfPadding = extraPadding/2;
+    //             for (let i=0;i<halfPadding;i++) {
+    //                 newString = ' ' + newString + ' ';
+    //             }
+    //         } else {
+    //             const almostHalfPadding = (extraPadding-1)/2;
+    //             for (let i=0;i<almostHalfPadding;i++) {
+    //                 newString = ' ' + newString + ' ';
+    //             };
+    //             newString = newString + ' ';
+    //         };
+    //         return newString;
+    //     }
+    // },
+    // Codecademy Solution
+    pad(string,length) {
+        if (length<=string.length) {
+            return string;
+        }
+        const startPaddingLength = Math.floor((length-string.length)/2);
+        const endPaddingLength = length-string.length-startPaddingLength;
+        const paddedString = ' '.repeat(startPaddingLength) + string + ' '.repeat(endPaddingLength);
+        return paddedString;
     }
 
 };
